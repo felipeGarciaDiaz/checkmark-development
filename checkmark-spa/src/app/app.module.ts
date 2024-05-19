b96a1a976625ac849024791f8635f30d5cacbbc7
felipe garcia diaz <felipedevsec@gmail.com>
2024-05-18 23:34:19 -0400

    fixed errors on angular

--- checkmark-spa/src/app/app.module.ts
+++ checkmark-spa/src/app/app.module.ts
@@ -1,68 +1,30 @@
-import {
-  APP_INITIALIZER,
-  NgModule,
-  CUSTOM_ELEMENTS_SCHEMA,
-} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthInterceptor } from './_services/interceptors/auth-interceptor';
-import { StorageService } from './_services/storage.service';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { LoginComponent } from './views/login/login.component';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module'; // Make sure this imports your routes correctly
+import { AppComponent } from './app.component';
+import { CurrencyPipe } from '@angular/common';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastComponent } from './components/toast/toast.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { MaterialModule } from './material.module';
-import { NavComponent } from './views/global/nav/nav.component';
-import { NgxPermissionsModule } from 'ngx-permissions';
-
-export function initializeApp(myService: StorageService) {
-  return () => myService.Init();
-}
-
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    ToastComponent,
-    DashboardComponent,
-    NavComponent
+
   ],
   imports: [
     CommonModule,
-    HttpClientModule,
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule,
+    AppRoutingModule, // This should be the only routing module you need here
+    HttpClientModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    NgxPermissionsModule.forRoot(),
-    
+    NoopAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeApp,
-      deps: [StorageService],
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-    provideAnimationsAsync(),
-  ],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [CurrencyPipe],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
