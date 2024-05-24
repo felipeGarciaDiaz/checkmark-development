cb8416a34d9569842e5124df39b5dddad085ce95
felipe garcia diaz <felipedevsec@gmail.com>
2024-05-24 00:07:53 -0400

    added search results page with data

--- checkmark-spa/src/app/views/results/results.component.ts
+++ checkmark-spa/src/app/views/results/results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from '../../_services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { ICopyrightMain } from '../../_models/copyrights';
@@ -7,14 +7,14 @@ import { ICopyrightMain } from '../../_models/copyrights';
   selector: 'app-results',
 
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
 })
 export class ResultsComponent implements OnInit {
 
 
 
-  public data: ICopyrightMain | undefined;
-  constructor(public api: ApiService, public routes: ActivatedRoute) { }
+  public data: ICopyrightMain[] = [];
+  constructor(public api: ApiService, public routes: ActivatedRoute, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.fetchSearchResults();
@@ -24,10 +24,12 @@ export class ResultsComponent implements OnInit {
     this.routes.queryParams.subscribe((params) => {
       const query = params['q'];
       this.api.getSearchRequests(query).subscribe({
-        next: (data: ICopyrightMain) => {
+        next: (data: ICopyrightMain[]) => {
           
           console.log(data);
           this.data = data;
+          this.cdr.detectChanges();  // Manually trigger change detection
+
         },
         error: (error: any) => {
           console.log(error);
